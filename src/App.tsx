3d9b865303009497a7075638525b193096ea511d
VladyslavHimself <vladyslavhimself@gmail.com>
2021-11-30 22:59:06 -0500

    moved timer to timerApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
 import classes from './App.module.scss';
+import { Timer } from './services/TimerAPI/Timer.service';
 
 const App = () => {
-  const [minutes, setMinutes] = useState<number>(25);
+  const [minutes, setMinutes] = useState<number>(0);
   const [seconds, setSeconds] = useState<number>(10);
   const [isTimerStarted, setIsTimerStarted] = useState<boolean>(false);
 
-
-  const tickTimer = () => {
-    const declaredTimerConstants = {
-      MINIMUM_SECOND_COUNT: 0,
-      MINIMUM_MINUTE_COUNT: 0,
-      RENEWED_MINUTES: 59,
-    }
-    const { MINIMUM_SECOND_COUNT, MINIMUM_MINUTE_COUNT, RENEWED_MINUTES } = declaredTimerConstants;
-
-    setTimeout(() => {
-      if (seconds <= MINIMUM_SECOND_COUNT) {
-        setSeconds(RENEWED_MINUTES);
-        setMinutes(minutes - 1);  
-      } else if (seconds > MINIMUM_MINUTE_COUNT) {
-        setSeconds(seconds - 1);
-      }
-   }, 1000);
-  }
+  const timer = new Timer();
 
   useEffect(() => {
     if (isTimerStarted) {
-     tickTimer();
+     timer.tick(seconds, minutes, setSeconds, setMinutes);
     }
-
   }, [minutes, seconds, isTimerStarted])
 
   const onStartTimerHandler = () => {
---
+++ src/services/TimerAPI/Timer.service.ts
@@ -0,0 +1,23 @@
+export class Timer {
+  
+  /**
+   * tick
+   */
+  public tick(seconds:number, minutes:number, changeSecond:any, changeMinute:any) {
+    const declaredTimerConstants = {
+      MINIMUM_SECOND_COUNT: 0,
+      MINIMUM_MINUTE_COUNT: 0,
+      RENEWED_MINUTES: 59,
+    }
+    const { MINIMUM_SECOND_COUNT, MINIMUM_MINUTE_COUNT, RENEWED_MINUTES } = declaredTimerConstants;
+    setTimeout(() => {
+      if (seconds <= MINIMUM_SECOND_COUNT) {
+        changeSecond(RENEWED_MINUTES);
+        changeMinute(minutes - 1);  
+      } else if (seconds > MINIMUM_MINUTE_COUNT) {
+        changeSecond(seconds - 1);
+      }
+   }, 1000);
+
+  }
+}
