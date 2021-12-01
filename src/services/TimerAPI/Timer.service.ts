51218337ebdf269fa6dc2de0ff7e64345d4d0578
VladyslavHimself <vladyslavhimself@gmail.com>
2021-12-01 10:32:41 -0500

    simplificated algorithm

--- src/services/TimerAPI/Timer.service.ts
+++ src/services/TimerAPI/Timer.service.ts
@@ -6,15 +6,15 @@ export class Timer {
   public tick(seconds:number, minutes:number, changeSecond:any, changeMinute:any) {
     const declaredTimerConstants = {
       MINIMUM_SECOND_COUNT: 0,
-      MINIMUM_MINUTE_COUNT: 0,
       RENEWED_MINUTES: 59,
     }
-    const { MINIMUM_SECOND_COUNT, MINIMUM_MINUTE_COUNT, RENEWED_MINUTES } = declaredTimerConstants;
+    const { MINIMUM_SECOND_COUNT, RENEWED_MINUTES } = declaredTimerConstants;
+
     setTimeout(() => {
       if (seconds <= MINIMUM_SECOND_COUNT) {
         changeSecond(RENEWED_MINUTES);
         changeMinute(minutes - 1);  
-      } else if (seconds > MINIMUM_MINUTE_COUNT) {
+      } else if (seconds > MINIMUM_SECOND_COUNT) {
         changeSecond(seconds - 1);
       }
    }, 1000);
