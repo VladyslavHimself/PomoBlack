898e5b234f5e243a196d392a8dbe17eac6d3c991
VladyslavHimself <vladyslavhimself@gmail.com>
2021-12-01 10:34:08 -0500

    added time format & timer pause

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import classes from './App.module.scss';
 import { Timer } from './services/TimerAPI/Timer.service';
 
 const App = () => {
-  const [minutes, setMinutes] = useState<number>(0);
-  const [seconds, setSeconds] = useState<number>(10);
+  const [minutes, setMinutes] = useState<number>(25);
+  const [seconds, setSeconds] = useState<number>(0);
   const [isTimerStarted, setIsTimerStarted] = useState<boolean>(false);
 
   const timer = new Timer();
 
   useEffect(() => {
     if (isTimerStarted) {
-     timer.tick(seconds, minutes, setSeconds, setMinutes);
+     minutes <= 0 && seconds <= 0 ? setIsTimerStarted(!isTimerStarted)
+     : timer.tick(seconds, minutes, setSeconds, setMinutes);
     }
   }, [minutes, seconds, isTimerStarted])
 
@@ -23,7 +24,7 @@ const App = () => {
   return (
     <div className={classes.app}>
       <span className={classes.timer}>
-        {minutes}:{seconds}
+        <span>{minutes < 10 ? (0) : null}{minutes}</span>:<span>{seconds < 10 ? (0) : null}{seconds}</span>
       </span>
       <button className={classes['start-button']} onClick={onStartTimerHandler}>Start timer</button>
     </div>
