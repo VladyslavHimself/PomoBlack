67c7350fee9d648ba385424c3f1402fa053c665a
VladyslavHimself <vladyslavhimself@gmail.com>
2021-12-01 20:44:17 -0500

    added quotes randomizer

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,31 @@ import React, { useEffect, useState } from 'react';
 import classes from './App.module.scss';
 import { TimerButton } from './components/Ui/TimerButton/TimerButton';
 import { Timer } from './services/TimerAPI/Timer.service';
-import girlImage from './girl.svg';
+import girlImage from './assets/girl.svg';
+import jsonQuotes from './assets/database/quotes.json';
+
+interface Quote {
+    id: number;
+    quote: string;
+}
+
+interface IQuote {
+    quotes: Quote[];
+}
 
 const App = () => {
   const [minutes, setMinutes] = useState<number>(25);
   const [seconds, setSeconds] = useState<number>(0);
   const [isTimerStarted, setIsTimerStarted] = useState<boolean>(false);
+  const [quote, setQuote] = useState<string>();
 
   const timer = new Timer();
+
+  useEffect(() => {
+    const randomId = Math.floor(Math.random() * jsonQuotes.quotes.length);
+    const quote = jsonQuotes.quotes[randomId];
+    setQuote(quote.quote);
+  }, []);
 
   useEffect(() => {
     if (isTimerStarted) {
@@ -26,7 +43,7 @@ const App = () => {
   return (
     <div className={classes.app}>
       <img className={classes['girl-image']} src={girlImage} alt="GirlWork" />
-      <h4 className={classes.quote}>- Lorem ipsum dolor sit amet. -</h4>
+      <h4 className={classes.quote}>- {quote} -</h4>
       <h1 className={classes.heading}>PomoBlack</h1>
       <span className={classes.timer}>
         <span>{minutes < 10 ? (0) : null}{minutes}</span>:<span>{seconds < 10 ? (0) : null}{seconds}</span>
